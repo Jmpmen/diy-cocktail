80f493afd93e1a5f0e4bdfa79db3620da8189d43
Jmpmen <mendozajamesmichael@gmail.com>
2022-11-10 23:58:29 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertTrigger = document.getElementById('liveAlertBtn')
+const cocktailSection = document.querySelector('.cocktails')
+
+document.querySelector('.searchButton').addEventListener('click', drinkRequest)
+document.querySelector('.randomButton').addEventListener('click', randomDrinkRequest)
+document.addEventListener('DOMContentLoaded', drinkRequest)
+
+const alertBootstrap = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.append(wrapper)
+  }
+
+async function drinkRequest(){
+    const cocktail = document.querySelector('input').value
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+    console.log(url)
+
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        clearDOM()
+
+        if (data.drinks === null){
+            alertBootstrap('Drink not found.','danger')
+        }else{
+            data.drinks.forEach(drinks => populateDrink(drinks))
+        }
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function randomDrinkRequest(){
+
+    try{
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        const data = await response.json()
+        console.log(data)
+        clearDOM()
+
+        data.drinks.forEach(drinks => populateDrink(drinks))
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+function populateDrink(drinks){
+    const cards = document.createElement('div')
+    cards.classList.add('col')
+
+    cards.innerHTML = `
+        <div class="card h-100 text">
+            <img src=${drinks.strDrinkThumb} class="card-img-top img-fluid" alt="${drinks.strDrinkThumb}">
+            <div class="card-body">
+                <h2 class="card-title text-white bg-success text-center">${drinks.strDrink}</h2>
+                <h3 class="text-success text-center">Ingredients</h3>
+                <ul class="text-start">${ingredients(drinks)}</ul>
+                <h3 class="text-success text-center">Instructions</h3>
+                <p>${drinks.strInstructions}</p>
+            </div>
+        </div>
+    `
+    cocktailSection.appendChild(cards)
+}
+
+function ingredients(drinks){
+    let str = ''
+    for (let i = 1; i<=15;i++){
+        const ingredients = `strIngredient${i}`
+        const measure = `strMeasure${i}`
+        if (drinks[ingredients]){
+        str += `<li>${drinks[measure] || ""} ${drinks[ingredients]}</li>`
+        }
+    }
+    console.log(str)
+    return str
+}
+
+function clearDOM(){
+    while (cocktailSection.firstChild)
+    cocktailSection.removeChild(cocktailSection.firstChild)
+
+    while (alertPlaceholder.firstChild)
+    alertPlaceholder.removeChild(alertPlaceholder.firstChild)
+}
