b5b7cde1dece57962d035713c652444b5ee08a3b
Jmpmen <mendozajamesmichael@gmail.com>
2022-11-11 01:35:12 +0800

    update

--- js/main.js
+++ js/main.js
@@ -59,7 +59,9 @@ function populateDrink(drinks){
 
     cards.innerHTML = `
         <div class="card h-100 text">
-            <img src=${drinks.strDrinkThumb} class="card-img-top" alt="${drinks.strDrinkThumb}">
+            <div>
+                <img src=${drinks.strDrinkThumb} class="card-img-top" alt="${drinks.strDrinkThumb}">
+            </div>
             <div class="card-body">
                 <h2 class="card-title text-white bg-success text-center">${drinks.strDrink}</h2>
                 <h3 class="text-success text-center">Ingredients</h3>
